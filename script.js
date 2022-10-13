d43267e35ed5e96586ebb34c025b770fe1627296
huzaifa-ai <official.huzaifa13@gmail.com>
2022-10-14 00:37:43 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      screens: {
+        sm: '480px',
+        md: '769px',
+        lg: '1100px',
+        xl: '1440px',
+        xxl: '2700px',
+      },
+    },
+  },
+};
